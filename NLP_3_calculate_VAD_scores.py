3a980898fe11d8ba28031800c8a4295cad2e057d
Leevi Rantala <leevirantala@gmail.com>
2018-06-11 15:44:56 +0300

    Booster calculations done
    Booster words from Sentistrength added
    to the calculations. This stage does not
    remove any words based on VAD (except booster
    words are omitted as they only modify the next word).

--- NLP_3_calculate_VAD_scores.py
+++ NLP_3_calculate_VAD_scores.py
@@ -7,6 +7,8 @@ from timeit import default_timer as timer
 import ast
 
 SKIPPED_WORDS = ["None", "be"]
+MILD_BOOSTER_WORDS = ["absolutely", "very", "really", "total", "totally", "especially", "definitely", "complete", "completely"]
+STRONG_BOOSTER_WORDS = ["extremely", "fuckin", "fucking", "hugely", "incredibly", "overwhelmingly", ]
 
 
 def open_file(file, type):
@@ -16,7 +18,7 @@ def open_file(file, type):
     else:
         logging.debug("Entering open file pandas")
         raw_table = pd.read_csv(file, sep=';', encoding='utf-8')
-    # This transforms the csv-string back to a list
+    # This transforms the csv-strings back to a list
         raw_table["related"] = raw_table["related"].map(ast.literal_eval)
         raw_table["opinion"] = raw_table["opinion"].map(ast.literal_eval)
         raw_table["aspect"] = raw_table["aspect"].map(ast.literal_eval)
@@ -50,70 +52,83 @@ def read_folder_contents(path_to_files):
     return filelist
 
 
-def calculate_vad_scores_for_phrases(raw_df):
+def booster_modification(booster_score, word_score):
+    if word_score <= 4:
+        word_score = word_score - booster_score
+    elif word_score >= 6:
+        word_score = word_score + booster_score
+    return word_score
+
+def calculate_vad_scores_for_nouns(raw_df):
+    pass
+
+def calculate_vad_scores_for_opinions(raw_df):
     """This assigns the opinion words as the base score for every
     asoect and then modifies it by calculating the related words in it."""
     logging.debug("Entering calculate new vad scores")
     df = raw_df
-    phrase_scores = []
-
-    # aspect, opinion and related temp tables
-    av = []
-    aa = []
-    ad = []
-    ov = []
-    oa = []
-    od = []
-    rv = []
-    ra = []
-    rd = []
+    opin_scores = []
+    related_scores = []
     start = timer()
+    opinion_related = ["opinion", "related"]
     for i, phrase in enumerate(df["aspect"]):
-        temp_a = []
-        temp_v = []
-        temp_d = []
-        if len(df["opinion"][i]) != 0:
-            temp_v.extend(df["opinion_v"][i])
-            temp_a.extend(df["opinion_a"][i])
-            temp_d.extend(df["opinion_d"][i])
+        opin_v = []
+        opin_a = []
+        opin_d = []
+        rela_v = []
+        rela_a = []
+        rela_d = []
+        for words in opinion_related:
+            if len(df[words][i]) != 0:
+                logging.debug((words) + "length %s" % str(len(df[words][i]  )))
+                j = 0
+                while j+1 < len(df[words][i]):
+                    if df[words][i][j] in MILD_BOOSTER_WORDS:
+                        df[words + "_v"][i][j+1] = booster_modification(0.5, df[words + "_v"][i][j+1])
+                        df[words + "_a"][i][j+1] = booster_modification(0.5, df[words + "_a"][i][j+1])
+                        df[words + "_d"][i][j+1] = booster_modification(0.5, df[words + "_d"][i][j+1])
+                        # Dangerous. Pops out a value. Works, but can mess things up royally.
+                        # df[words + "_v"][i].pop(j)
+                    elif df[words][i][j] in STRONG_BOOSTER_WORDS:
+                        df[words + "_v"][i][j+1] = booster_modification(1, df[words + "_v"][i][j+1])
+                        df[words + "_a"][i][j+1] = booster_modification(1, df[words + "_a"][i][j+1])
+                        df[words + "_d"][i][j+1] = booster_modification(1, df[words + "_d"][i][j+1])
+                    j += 1
+                if words == "opinion":
+                    k = 0
+                    while k < len(df[words][i]):
+                        print(df[words][i][k])
+                        if df[words][i][k] not in MILD_BOOSTER_WORDS + STRONG_BOOSTER_WORDS:
+                            opin_v.append(df[words + "_v"][i][k])
+                            opin_a.append(df[words + "_a"][i][k])
+                            opin_d.append(df[words + "_d"][i][k])
+                        k+=1
+                else:
+                    k = 0
+                    while k < len(df[words][i]):
+                        print(df[words][i][k])
+                        if df[words][i][k] not in MILD_BOOSTER_WORDS + STRONG_BOOSTER_WORDS:
+                            rela_v.append(df[words + "_v"][i][k])
+                            rela_a.append(df[words + "_a"][i][k])
+                            rela_d.append(df[words + "_d"][i][k])
+                        k+=1
+        if len(opin_v) != 0:
+            new_v = float(format(sum(opin_v)/len(opin_v), '.2f'))
+            new_a = float(format(sum(opin_a) / len(opin_a), '.2f'))
+            new_d = float(format(sum(opin_d) / len(opin_d), '.2f'))
+            opin_scores.append((new_v, new_a, new_d))
+        if len(rela_v) != 0:
+            new_v = float(format(sum(rela_v)/len(rela_v), '.2f'))
+            new_a = float(format(sum(rela_a) / len(rela_a), '.2f'))
+            new_d = float(format(sum(rela_d) / len(rela_d), '.2f'))
+            related_scores.append((new_v, new_a, new_d))
 
-        if len(temp_v) != 0:
-            new_v = float(format(sum(temp_v)/len(temp_v), '.2f'))
-            new_a = float(format(sum(temp_a) / len(temp_a), '.2f'))
-            new_d = float(format(sum(temp_d) / len(temp_d), '.2f'))
-            phrase_scores.append((new_v, new_a, new_d))
-    df_scores = pd.DataFrame.from_records(phrase_scores, columns=("new_v", "new_a", "new_d"))
-    df = pd.concat([df, df_scores], axis=1, sort=False)
-    #     for word, v, a, d in phrase:
-    #         new_word.append(word)
-    #         valence.append(v)
-    #         arousal.append(a)
-    #         dominance.append(d)
-    #     for word, v, a, d in (new_adjectives[i]):
-    #         if word not in SKIPPED_WORDS:
-    #             if v < 4 or v > 6:
-    #                 valence.append(v)
-    #                 arousal.append(a)
-    #                 dominance.append(d)
-    #             elif a < 4 or a > 6:
-    #                 valence.append(v)
-    #                 arousal.append(a)
-    #                 dominance.append(d)
-    #             elif d < 4 or d > 6:
-    #                 valence.append(v)
-    #                 arousal.append(a)
-    #                 dominance.append(d)
-    #     new_string = ' '.join(new_word).lower()
-    #     new_valence = float(format(sum(valence)/len(valence), '.2f'))
-    #     new_arousal = float(format(sum(arousal)/len(arousal), '.2f'))
-    #     new_dominance = float(format(sum(dominance)/len(dominance), '.2f'))
-    #     original_scores.append(phrase)
-    #     phrase_scores.append((new_string, str(new_valence), str(new_arousal), str(new_dominance)))
-    # df_scores = pd.DataFrame.from_records(phrase_scores, columns=("clean_phrase", "valence", "arousal", "dominance"))
-    # old_scores = pd.Series(original_scores)
-    # df_scores["single_words"] = old_scores.values
-    # end = timer()
-    # logging.debug("Time: %.2f seconds" % (end - start))
+    df_oscores = pd.DataFrame.from_records(opin_scores, columns=("opin_new_v", "opin_new_a", "opin_new_d"))
+    df_rscores = pd.DataFrame.from_records(related_scores, columns=("rela_new_v", "rela_new_a", "rela_new_d"))
+    df = pd.concat([df, df_oscores], axis=1, sort=False)
+    df = pd.concat([df, df_rscores], axis=1, sort=False)
+    end = timer()
+    logging.debug("Time: %.2f seconds" % (end - start))
     return df
 
 
@@ -125,7 +140,7 @@ def return_sys_arguments(args):
 
 
 def main(df_part, name):
-    df_vad_scores = calculate_vad_scores_for_phrases(df_part)
+    df_vad_scores = calculate_vad_scores_for_opinions(df_part)
     save_file(df_vad_scores, name + "_CALCULATED_R")
     # result = pd.concat([df_vad_scores, new_df], axis=1, sort=False)
     # result = separate_individual_words(result, True)
